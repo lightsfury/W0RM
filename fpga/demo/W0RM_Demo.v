c7c8d7a39eeeadfcdda3972f660028a1683ca381
Robert Beam <robertbeam@gmail.com>
2015-04-30 15:04:30 -0700

    Removed old code in W0RM_Demo.v.

--- fpga/demo/W0RM_Demo.v
+++ fpga/demo/W0RM_Demo.v
@@ -13,19 +13,12 @@ module W0RM_Demo(
   localparam  ADDR_WIDTH  = 32;
   
   wire  [ADDR_WIDTH-1:0]  inst_addr_o;
-  wire  [DATA_WIDTH-1:0]  inst_data;
   wire  [DATA_WIDTH-1:0]  gpio_bus_data_i;
   wire                    gpio_bus_valid_i;
-  reg                     inst_valid_r1 = 0, 
-                          inst_valid_r2 = 0;
-  reg   [ADDR_WIDTH-1:0]  inst_addr_r1  = 0,
-                          inst_addr_r2  = 0;
-  reg   [INST_WIDTH-1:0]  inst_data_r   = 0;
   
   wire  [ADDR_WIDTH-1:0]  mem_addr_o;
   wire  [DATA_WIDTH-1:0]  mem_data_o,
                           mem_data_i;
-  //reg                     mem_valid_i = 0;
   
   wire  [DATA_WIDTH-1:0]  gpio_a_data_i;
   wire                    gpio_a_valid_i;
@@ -41,17 +34,14 @@ module W0RM_Demo(
   wire  [ADDR_WIDTH-1:0]  inst_rom_1_addr_o,
                           inst_rom_2_addr_o,
                           inst_rom_3_addr_o,
-                          inst_rom_23_addr_o,
                           inst_rom_addr_o;
   wire  [INST_WIDTH-1:0]  inst_rom_1_data_o,
                           inst_rom_2_data_o,
                           inst_rom_3_data_o,
-                          inst_rom_23_data_o,
                           inst_rom_data_o;
   wire                    inst_rom_1_valid_o,
                           inst_rom_2_valid_o,
                           inst_rom_3_valid_o,
-                          inst_rom_23_valid_o,
                           inst_rom_valid_o;
   
   IBUFGDS sysclk_buffer(
@@ -76,28 +66,14 @@ module W0RM_Demo(
   always @(posedge core_clk)
     reset_r <= ~reset_i || ~pll_locked;
   
-  /*
-  always @(posedge core_clk)
-  begin
-    inst_valid_r2 <= inst_valid_r1;
-    inst_valid_r1 <= inst_valid_o;
-    
-    inst_addr_r2  <= inst_addr_r1;
-    inst_addr_r1  <= inst_addr_o;
-    
-    if (inst_addr_r1[1])
-      inst_data_r <= inst_data[INST_WIDTH-1:0];
-    else
-      inst_data_r <= inst_data[DATA_WIDTH-1:(DATA_WIDTH-INST_WIDTH)];
-  end // */
-  
   W0RM_Peripheral_MemoryBlock #(
     .ADDR_WIDTH(ADDR_WIDTH),
     .DATA_WIDTH(INST_WIDTH),
     .MEM_DEPTH(256),
     .BASE_ADDR(32'h2000_0000),
     .INIT_FILE("../demo/programs/boot-loader.hex"),
-    .USER_WIDTH(ADDR_WIDTH)
+    .USER_WIDTH(ADDR_WIDTH),
+    .USE_BRAM(0)
   ) boot_loader_rom (
     .mem_clk(core_clk),
     .cpu_reset(reset_r),
@@ -119,7 +95,8 @@ module W0RM_Demo(
     .MEM_DEPTH(256),
     .BASE_ADDR(32'h2100_0000),
     .INIT_FILE("../demo/programs/blink-led.hex"),
-    .USER_WIDTH(ADDR_WIDTH)
+    .USER_WIDTH(ADDR_WIDTH),
+    .USE_BRAM(0)
   ) prog_0_rom (
     .mem_clk(core_clk),
     .cpu_reset(reset_r),
@@ -141,7 +118,8 @@ module W0RM_Demo(
     .MEM_DEPTH(256),
     .BASE_ADDR(32'h2200_0000),
     .INIT_FILE("../demo/programs/switch-copy.hex"),
-    .USER_WIDTH(ADDR_WIDTH)
+    .USER_WIDTH(ADDR_WIDTH),
+    .USE_BRAM(0)
   ) prog_1_rom (
     .mem_clk(core_clk),
     .cpu_reset(reset_r),
@@ -177,48 +155,6 @@ module W0RM_Demo(
     .bus_valid_o(inst_rom_valid_o),
     .bus_data_o({inst_rom_data_o, inst_rom_addr_o})
   );
-  /*
-  W0RM_Peripheral_Bus_Extender #(
-    .DATA_WIDTH(ADDR_WIDTH + INST_WIDTH)
-  ) inst_123_mux (
-    .bus_clock(core_clk),
-    
-    .bus_port0_valid_i(inst_rom_1_valid_o),
-    .bus_port0_data_i({inst_rom_1_data_o, inst_rom_1_addr_o}),
-    
-    .bus_port1_valid_i(inst_rom_23_valid_o),
-    .bus_port1_data_i({inst_rom_23_data_o, inst_rom_23_addr_o}),
-    
-    .bus_valid_o(inst_rom_valid_o),
-    .bus_data_o({inst_rom_data_o, inst_rom_addr_o})
-  );
-  
-  W0RM_Peripheral_Bus_Extender #(
-    .DATA_WIDTH(ADDR_WIDTH + INST_WIDTH)
-  ) inst_23_mux (
-    .bus_clock(core_clk),
-    
-    .bus_port0_valid_i(inst_rom_2_valid_o),
-    .bus_port0_data_i({inst_rom_2_data_o, inst_rom_2_addr_o}),
-    
-    .bus_port1_valid_i(inst_rom_3_valid_o),
-    .bus_port1_data_i({inst_rom_3_data_o, inst_rom_3_addr_o}),
-    
-    .bus_valid_o(inst_rom_23_valid_o),
-    .bus_data_o({inst_rom_23_data_o, inst_rom_23_addr_o})
-  );
-  // */
-  
-  /*
-  W0RM_Example_Design_Instruction_ROM example_rom(
-    .clka(core_clk),
-    .rsta(reset_r),
-    
-    .ena(1'b1),
-    .addra(inst_addr_o),
-    
-    .douta(inst_data)
-  ); // */
   
   // W0RM CPU core
   W0RM_TopLevel #(
@@ -366,22 +302,4 @@ module W0RM_Demo(
     .mem_a_user_i(1'b0),
     .mem_a_user_o() // Not used
   );
-  
-  /*
-  W0RM_CoreRAM_Block main_memory(
-    // Port A is not used in this example
-    .clka(1'b0),
-    .ena(1'b0),
-    .wea(1'b0),
-    .addra(11'd0),
-    .dina(16'd0),
-    .douta(), // Not used
-    
-    .clkb(core_clk),
-    .enb(mem_valid_o && (mem_read_o || mem_write_o)),
-    .web(mem_valid_o && mem_write_o),
-    .addrb(mem_addr_o[11:2]),
-    .dinb(mem_data_o),
-    .doutb(mem_data_i)
-  ); // */
 endmodule
